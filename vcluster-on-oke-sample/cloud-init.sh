5416520b251f097c092d6f5e73b0a3716d877b52
Daniel Dinu <daniel.dinu@oracle.com>
2026-03-23 12:57:34 +0200

    Update cloud-init.sh

--- vcluster-on-oke-sample/cloud-init.sh
+++ vcluster-on-oke-sample/cloud-init.sh
@@ -1,7 +1,7 @@
+#!/bin/sh
+
 # Copyright (c) 2024, 2026, Oracle and/or its affiliates. All rights reserved.
 # The Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl/
-
-#!/bin/sh
 
 export OCI_CLI_AUTH=instance_principal
 cluster_id=$(curl -s -H "Authorization: Bearer Oracle" http://169.254.169.254/opc/v1/instance/metadata/cluster_id)
